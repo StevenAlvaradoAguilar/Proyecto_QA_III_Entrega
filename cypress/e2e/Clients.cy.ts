6c5309982b86bff8e55cbcb523b3a969c88add87
Steven <alvaradosteven6@gmail.com>
2022-11-07 14:40:12 -0600

    Implementación de casos

--- cypress/e2e/Clients.cy.ts
+++ cypress/e2e/Clients.cy.ts
@@ -11,7 +11,7 @@ describe('Clients', function () {
         cy.get('.btn > .m-0').click();
         cy.get('.list-group > [href="#projectsMenu"]').click();
         cy.get('.list-group > #projectsMenu > [href="/clients"]').click();
-        cy.get('.col-md-6 > :nth-child(1)').click();
+        cy.get('.col-md-2 > :nth-child(1)').click();
         cy.get('.pmo-content').click().then(
             function(){
                 cy.get('.invalid-feedback').should('contain.text', "This field is required");
