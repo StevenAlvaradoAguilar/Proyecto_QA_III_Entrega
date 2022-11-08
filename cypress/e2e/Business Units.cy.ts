8c8c443a43c63adde6c9d50b57f5f43b32ee9a96
Steven <alvaradosteven6@gmail.com>
2022-11-08 13:42:28 -0600

    Implementación de casos y arreglos nuevos

--- cypress/e2e/Business Units.cy.ts
+++ cypress/e2e/Business Units.cy.ts
@@ -3,8 +3,8 @@ describe('Business Units', function () {
         cy.visit('https://tec.gacela.io/business_units');
     });
 
-    //Nombre: 'Description con espacio en blanco en Business Units'
-    //Codigo: CP-21, CP-39
+    // Nombre: 'Description con espacio en blanco en Business Units'
+    // Codigo: CP-21, CP-39
     it('Description con espacio en blanco en Business Units', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
@@ -13,6 +13,23 @@ describe('Business Units', function () {
         cy.get('.list-group > #projectsMenu > [href="/business_units"]').click();
         cy.get('.text-right > :nth-child(1)').click();
         cy.get('#name').type(' ');
+        cy.get('.pmo-content').click().then(
+            function(){
+               cy.get('.invalid-feedback').should('contain.text', "This field is required");
+            }
+        );
+    });
+    
+    // Nombre: 'Description con con un nombre para buscar en Business Units'
+    // Codigo: CP-22, CP-40
+    it('Description con con un nombre para buscar en Business Units', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+        cy.get('.list-group > [href="#projectsMenu"]').click();
+        cy.get('.list-group > #projectsMenu > [href="/business_units"]').click();
+        cy.get('.text-right > :nth-child(1)').click();
+        cy.get('#name').type('Ejemplo');
         cy.get('.pmo-content').click().then(
             function(){
                cy.get('.invalid-feedback').should('contain.text', "This field is required");
--- cypress/e2e/Clients.cy.ts
+++ cypress/e2e/Clients.cy.ts
@@ -18,6 +18,23 @@ describe('Clients', function () {
             }
         );
     });
+
+    //Nombre de la prueba: Descripcion con el nombre de un Clients
+    //Codigo: CP-7, CP-47
+    it('Description con el nombre de un Clients', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+        cy.get('.list-group > [href="#projectsMenu"]').click();
+        cy.get('.list-group > #projectsMenu > [href="/clients"]').click();
+        cy.get('.col-md-2 > :nth-child(1)').click();
+        cy.get('#name').type('Hoalala');
+        cy.get('.pmo-content').click().then(
+            function(){
+                cy.get('.invalid-feedback').should('contain.text', "This field is required");
+            }
+        );
+    });
     
     afterEach(function () {
         cy.reload();
