0d6b0db403ca6fc30bda02ff959f30e302fe8aec
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-09 22:39:28 -0600

    Caso de prueba en Cost Per Resource

--- cypress/e2e/Costs Per Resource.cy.ts
+++ cypress/e2e/Costs Per Resource.cy.ts
@@ -3,6 +3,18 @@ describe('Costs Per Resource', function () {
         cy.visit('https://tec.gacela.io/cost_per_user');
     });
 
+    //Nombre de la prueba: User con valor desconocido en Costs Per Resource
+    //Codigo: CP-24, CP-52
+    //Acierta, porque se espera que al buscar un usuario retorne empty
+    it('User con valor desconocido en Costs Per Resource', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+        cy.get('.list-group > [href="#projectsMenu"]').click();
+        cy.get('.list-group > #projectsMenu > [href="/cost_per_user"]').click();
+        cy.get('.vs__search').type("Admin2").should("be.empty")
+    });
+
     //Nombre de la prueba: Date con espacio en blanco en Costs Per Resource
     //Codigo: CP-23, CP-51
     //Falla, porque el boton no esta habilitado
@@ -40,7 +52,6 @@ describe('Costs Per Resource', function () {
             }
         );
     });
-
     afterEach(function () {
         cy.reload();
     });
