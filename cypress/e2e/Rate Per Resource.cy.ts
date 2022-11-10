421e91af1068f65180aa071746dd4bfa4084c554
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 17:39:50 -0600

    Nuevo caso

--- cypress/e2e/Rate Per Resource.cy.ts
+++ cypress/e2e/Rate Per Resource.cy.ts
@@ -2,6 +2,19 @@ describe('Rate Per User', function () {
     beforeEach(function () {
         cy.visit('https://tec.gacela.io/rate_per_user');
     });
+    it('Project con espacio en blanco en Rate Per User', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+        cy.get('.list-group > [href="#projectsMenu"]').click();
+        cy.get('.list-group > #projectsMenu > [href="/rate_per_user"]').click();
+        cy.get('.vs__selected-options').type("Poject1")
+        cy.wait(2000)
+        cy.get('.vs__selected-options').type('io{enter}')
+        cy.get('.btn').click()
+        cy.get(':nth-child(3) > .form-control').type("50000")
+        cy.get('.btn-company-secondary').should('be.disabled');
+    });
 
     //Nombre de la prueba: Project con espacio en blanco en Rate Per User
     //Codigo: CP-23, CP-51
