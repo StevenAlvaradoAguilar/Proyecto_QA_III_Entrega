906afc57d6cac5f5103aacb9d6cf8a2c22d65ff7
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-11-10 12:53:49 -0600

    EditEquipments & PersonalGrowth Changes

--- cypress/e2e/EditEquipments.cy.ts
+++ cypress/e2e/EditEquipments.cy.ts
@@ -1,116 +1,115 @@
 describe('Learning Paths', function () {
   beforeEach(function () {
-    cy.visit('https://tec.gacela.io/learning_paths');
+    cy.visit('https://tec.gacela.io/');
   });
 
-//   //Nombre de la prueba: Editanto Equipment Internal Key vacía botón Save desabilitado
-//   //Codigo: CP-55
-//   it('Editanto Equipment Internal Key vacía botón, Save desabilitado', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+  //Nombre de la prueba: Editanto Equipment Internal Key vacía botón Save desabilitado
+  //Codigo: CP-55
+  it('Editanto Equipment Internal Key vacía botón, Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
-//   //Nombre de la prueba: Editanto Equipment Serial Number vacío botón Save desabilitado
-//   //Codigo: CP-56
-//   it('Editanto Equipment Serial Number vacío, botón Save desabilitado', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
-//   //Nombre de la prueba: Editanto Equipment Internal Key vacía botón, Solicita Internal Key
-//   //Codigo: CP-57
-//   it('Editanto Equipment Internal Key vacía botón, Solicita Internal Key', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+    cy.get('.card-body > .row > :nth-child(3) .form-control').clear();
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+
+});
+
+  //Nombre de la prueba: Editanto Equipment Serial Number vacío botón Save desabilitado
+  //Codigo: CP-56
+  it('Editanto Equipment Serial Number vacío, botón Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(4) .form-control').clear();
+
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+});
+
+
+  //Nombre de la prueba: Editanto Equipment Internal Key vacía botón, Solicita Internal Key
+  //Codigo: CP-57
+  it('Editanto Equipment Internal Key vacía botón, Solicita Internal Key', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(3) .form-control').clear().then(
+
+        function(){
+            cy.get(':nth-child(3) > span > .invalid-feedback').should('contain.text', "This field is required");
+        }
+    );
+});
+
+
 //   //Nombre de la prueba: Editanto Equipment Serial Number vacío, Solicita Serial Number
 //   //Codigo: CP-58
-//   it('Editanto Equipment Serial Number vacío, Solicita Serial Number', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+  it('Editanto Equipment Serial Number vacío, Solicita Serial Number', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
-//   //Nombre de la prueba: Editanto Equipment Model vacío, botón Save desabilitado
-//   //Codigo: CP-59
-//   it('Editanto Equipment Model vacío, botón Save desabilitado', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
-//   //Nombre de la prueba: Editanto Equipment Description vacía, botónSave desabilitado
-//   //Codigo: CP-60
-//   it('Editanto Equipment Description vacía, botón Save desabilitado', function () {
-//     cy.get('#email').type('qatec2022@gmail.com');
-//     cy.get('#password').type('qatecjnsc2022');
-//     cy.get('.btn > .m-0').click();
-//     cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-//     cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+    cy.get('.card-body > .row > :nth-child(4) .form-control').clear().then(
 
-    
-//     cy.get('.col-md-3 > :nth-child(1)').click()
-//     cy.get('#serial_number').click()
-//     cy.get('.card-body').click().then(
-//         function(){
-//             cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
-//         }
-//     );
-// });
+      function(){
+          cy.get(':nth-child(4) > span > .invalid-feedback').should('contain.text', "This field is required");
+      }
+  );
+
+});
+
+
+  //Nombre de la prueba: Editanto Equipment Model vacío, botón Save desabilitado
+  //Codigo: CP-59
+  it('Editanto Equipment Model vacío, botón Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(5) .form-control').clear();
+
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+});
+
+  //Nombre de la prueba: Editanto Equipment Description vacía, botónSave desabilitado
+  //Codigo: CP-60
+  it('Editanto Equipment Description vacía, botón Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(6) .trix-content').clear();
+
+
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+});
 
 
   afterEach(function () {
