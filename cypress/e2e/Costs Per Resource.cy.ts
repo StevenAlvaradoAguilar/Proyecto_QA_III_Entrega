518dc6b4de48cf52397b271eaf35bd10c1167211
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 20:34:45 -0600

    Documentacion casos de Costs Per Resource

--- cypress/e2e/Costs Per Resource.cy.ts
+++ cypress/e2e/Costs Per Resource.cy.ts
@@ -3,9 +3,15 @@ describe('Costs Per Resource', function () {
         cy.visit('https://tec.gacela.io/cost_per_user');
     });
 
-    //Nombre de la prueba: User con valor desconocido en Costs Per Resource
-    //Codigo: CP-24, CP-52
+    
     //Acierta, porque se espera que al buscar un usuario retorne empty
+    /*
+    Nombre de la prueba: User con valor desconocido en Costs Per Resource
+    Objetivo: Probar la funcionalidad al agregar un costo en Costs Per Resource
+    Datos de prueba: User: Admin2
+    Resultado esperado: Exito, porque se espera que al buscar un usuario retorne empty
+                        ya que ese usuario no existe.
+    */
     it('User con valor desconocido en Costs Per Resource', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
@@ -20,6 +26,14 @@ describe('Costs Per Resource', function () {
     //Nombre de la prueba: Date con espacio en blanco en Costs Per Resource
     //Codigo: CP-23, CP-51
     //Falla, porque el boton no esta habilitado
+
+    /*
+    Nombre de la prueba: Date con espacio en blanco en Costs Per Resource
+    Objetivo: Probar la funcionalidad al agregar un costo en Costs Per Resource
+    Datos de prueba: User: admin
+    Resultado esperado: Exito, porque se espera que al buscar un usuario retorne empty
+                        ya que ese usuario no existe.
+    */
     it('Date con espacio en blanco en Costs Per Resource', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
@@ -35,9 +49,13 @@ describe('Costs Per Resource', function () {
         cy.get('.btn').click()
     });
 
-    //Nombre de la prueba: Cost con espacio en blanco en Costs Per Resource
-    //Codigo: CP-24, CP-52
-    //Falla, porque el boton no esta habilitado
+    /*
+    Nombre de la prueba: Cost con espacio en blanco en Costs Per Resource
+    Objetivo: Probar la funcionalidad al agregar un costo en Costs Per Resource
+    Datos de prueba: User: admin
+    Resultado esperado: Exito, porque se espera que al buscar un usuario retorne empty
+                        ya que ese usuario no existe.
+    */
     it('Cost con espacio en blanco en Costs Per Resource', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
