ba28d74d42e228dfa4314b7592cd3b6b6d4b5604
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 20:18:16 -0600

    Documentacion casos de Clients

--- cypress/e2e/Clients.cy.ts
+++ cypress/e2e/Clients.cy.ts
@@ -3,8 +3,13 @@ describe('Clients', function () {
         cy.visit('https://tec.gacela.io/clients');
     });
 
-    //Nombre de la prueba: Descripcion con espacio blanco en Clients
-    //Codigo: CP-7, CP-47
+    /*
+    Nombre de la prueba: Descripcion con espacio blanco en Clients
+    Objetivo: Probar la funcionalidad agregando un Client
+    Datos de prueba: Description: ----
+    Resultado esperado: Exito ya que al ingresar un Clients sin descripcion deberia de 
+                        devolver "This field is required"
+    */
     it('Description con espacio en blanco en Clients', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
@@ -23,9 +28,14 @@ describe('Clients', function () {
         );
     });
 
-    //Nombre de la prueba: Descripcion con el nombre de un Clients
-    //Codigo: CP-7, CP-47
-    it('Description con el nombre de un Clients', function () {
+    /*
+    Nombre de la prueba: Descripcion con el nombre de un Clients y sin Type
+    Objetivo: Probar la funcionalidad agregando un Client
+    Datos de prueba: Description: This is my first client
+    Resultado esperado: Exito ya que al ingresar un Clients con descripcion pero sin el Type 
+                        deberia de devolver "This field is required"
+    */
+    it('Description con el nombre de un Clients y sin Type', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
         cy.get('.btn > .m-0').click();
@@ -34,7 +44,7 @@ describe('Clients', function () {
         cy.get('.list-group > #projectsMenu > [href="/clients"]').click();
 
         cy.get('.text-right > :nth-child(1)').click();
-        cy.get('#description').type('Hoalala');
+        cy.get('#description').type('This is my first client');
 
         cy.get('.pmo-content').click().then(
             function(){
