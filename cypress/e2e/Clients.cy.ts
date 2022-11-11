ae04e17312294248503fa95bb047562f1ea563f1
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 21:51:06 -0600

    Fixes

--- cypress/e2e/Clients.cy.ts
+++ cypress/e2e/Clients.cy.ts
@@ -2,6 +2,30 @@ describe('Clients', function () {
     beforeEach(function () {
         cy.visit('https://tec.gacela.io/clients');
     });
+    /*
+    Nombre de la prueba: Descripcion con el nombre de un Clients y sin Type
+    Objetivo: Probar la funcionalidad agregando un Client
+    Datos de prueba: Description: This is my first client
+    Resultado esperado: Exito ya que al ingresar un Clients con descripcion pero sin el Type 
+                        deberia de devolver "This field is required"
+    */
+    it('Description con el nombre de un Clients y sin Type', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+
+        cy.get('.list-group > [href="#projectsMenu"]').click();
+        cy.get('.list-group > #projectsMenu > [href="/clients"]').click();
+
+        cy.get('.text-right > :nth-child(1)').click();
+        cy.get('#description').type('This is my first client');
+
+        cy.get('.pmo-content').click().then(
+            function(){
+                cy.get('.invalid-feedback').should('contain.text', "This field is required");
+            }
+        );
+    });
 
     /*
     Nombre de la prueba: Descripcion con espacio blanco en Clients
@@ -21,31 +45,6 @@ describe('Clients', function () {
         cy.get('.text-right > :nth-child(1)').click();
         cy.get('#description').type(' ');
         
-        cy.get('.pmo-content').click().then(
-            function(){
-                cy.get('.invalid-feedback').should('contain.text', "This field is required");
-            }
-        );
-    });
-
-    /*
-    Nombre de la prueba: Descripcion con el nombre de un Clients y sin Type
-    Objetivo: Probar la funcionalidad agregando un Client
-    Datos de prueba: Description: This is my first client
-    Resultado esperado: Exito ya que al ingresar un Clients con descripcion pero sin el Type 
-                        deberia de devolver "This field is required"
-    */
-    it('Description con el nombre de un Clients y sin Type', function () {
-        cy.get('#email').type('qatec2022@gmail.com');
-        cy.get('#password').type('qatecjnsc2022');
-        cy.get('.btn > .m-0').click();
-
-        cy.get('.list-group > [href="#projectsMenu"]').click();
-        cy.get('.list-group > #projectsMenu > [href="/clients"]').click();
-
-        cy.get('.text-right > :nth-child(1)').click();
-        cy.get('#description').type('This is my first client');
-
         cy.get('.pmo-content').click().then(
             function(){
                 cy.get('.invalid-feedback').should('contain.text', "This field is required");
