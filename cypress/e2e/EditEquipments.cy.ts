7cf870206156315cc933281bd852d90b5d2d7ff4
CristopherSC <cristophersc2815956@estudiantec.cr>
2022-11-10 20:08:38 -0600

    41-60

--- cypress/e2e/EditEquipments.cy.ts
+++ cypress/e2e/EditEquipments.cy.ts
@@ -4,7 +4,7 @@ describe('Learning Paths', function () {
   });
 
   //Nombre de la prueba: Editanto Equipment Internal Key vacía botón Save desabilitado
-  //Codigo: CP-55
+  //Codigo: CP-48
   it('Editanto Equipment Internal Key vacía botón, Save desabilitado', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -21,26 +21,8 @@ describe('Learning Paths', function () {
 
   });
 
-  //Nombre de la prueba: Editanto Equipment Serial Number vacío botón Save desabilitado
-  //Codigo: CP-56
-  it('Editanto Equipment Serial Number vacío, botón Save desabilitado', function () {
-    cy.get('#email').type('qatec2022@gmail.com');
-    cy.get('#password').type('qatecjnsc2022');
-    cy.get('.btn > .m-0').click();
-
-    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
-    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
-
-    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
-
-    cy.get('.card-body > .row > :nth-child(4) .form-control').clear();
-
-    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
-  });
-
-
-  //Nombre de la prueba: Editanto Equipment Internal Key vacía botón, Solicita Internal Key
-  //Codigo: CP-57
+   //Nombre de la prueba: Editanto Equipment Internal Key vacía botón, Solicita Internal Key
+  //Codigo: CP-49
   it('Editanto Equipment Internal Key vacía botón, Solicita Internal Key', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -58,8 +40,28 @@ describe('Learning Paths', function () {
         }
     );
   });
+
+
+  //Nombre de la prueba: Editanto Equipment Serial Number vacío botón Save desabilitado
+  //Codigo: CP-50
+  it('Editanto Equipment Serial Number vacío, botón Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(4) .form-control').clear();
+
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+  });
+
+
   //Nombre de la prueba: Editanto Equipment Serial Number vacío, Solicita Serial Number
-  //Codigo: CP-58
+  //Codigo: CP-51
   it('Editanto Equipment Serial Number vacío, Solicita Serial Number', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -80,7 +82,7 @@ describe('Learning Paths', function () {
 
 
   //Nombre de la prueba: Editanto Equipment Model vacío, botón Save desabilitado
-  //Codigo: CP-59
+  //Codigo: CP-52
   it('Editanto Equipment Model vacío, botón Save desabilitado', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -96,8 +98,32 @@ describe('Learning Paths', function () {
     cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
   });
 
-  //Nombre de la prueba: Editanto Equipment Description vacía, botónSave desabilitado
-  //Codigo: CP-60
+  //Nombre de la prueba: Editanto Equipment Model vacío, Solicita Model
+  //Codigo: CP-53
+  it('Editanto Equipment Model vacío, Solicita Model', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(5) .form-control').clear().then(
+
+      function(){
+          cy.get(':nth-child(5) > span > .invalid-feedback').should('contain.text', "This field is required");
+      }
+    );
+  });
+
+
+
+
+
+  //Nombre de la prueba: Editanto Equipment Description vacía, botón Save desabilitado
+  //Codigo: CP-54
   it('Editanto Equipment Description vacía, botón Save desabilitado', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -114,7 +140,44 @@ describe('Learning Paths', function () {
     cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
   });
 
+  //Nombre de la prueba: Editanto Equipment Description vacía, Solicita descripción
+  //Codigo: CP-55
+  it('Editanto Equipment Description vacía, Solicita descripción', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
 
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > .row > :nth-child(6) .trix-content').clear().then(
+
+      function(){
+          cy.get(':nth-child(5) > span > .invalid-feedback').should('contain.text', "This field is required");
+      }
+    );
+  });
+
+
+   //Nombre de la prueba: Editanto Equipment Sin fecha, botón Save desabilitado
+  //Codigo: CP-56
+  it('Editanto Equipment Sin fecha, botón Save desabilitado', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+
+    cy.get('.list-group > [href="#inventoryMenu"] > .d-flex').click();
+    cy.get('.list-group > #inventoryMenu > .list-group-item').click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.vd-picker__input-clear__icon').click();
+
+    cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
+  });
+  
   afterEach(function () {
     cy.reload();
   });
--- cypress/e2e/EditPathLearnings.cy.ts
+++ cypress/e2e/EditPathLearnings.cy.ts
@@ -4,7 +4,7 @@ describe('Learning Paths', function () {
   });
 
    //Nombre de la prueba: Editar Learning Path con espacio en blanco en NAME y con VERSION completo Boton Save desabilitado
-   //Codigo: CP-53
+   //Codigo: CP-57
   it('Editar Learning Path con espacio en blanco en NAME y con VERSION completo Boton Save desabilitado', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -22,8 +22,31 @@ describe('Learning Paths', function () {
     cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
   });
 
+     //Nombre de la prueba: Editar Learning Path con espacio en blanco en NAME, Solicita el NAME
+   //Codigo: CP-58
+   it('Editar Learning Path con espacio en blanco en NAME, Solicita el NAME', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#LearningPathMenu"]').click();
+    cy.get(
+      '.list-group > #LearningPathMenu > [href="/learning_paths"]'
+    ).click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > :nth-child(1) #name').clear().then(
+
+
+      function(){
+          cy.get('.row > :nth-child(1) .card-body > :nth-child(1)  .invalid-feedback').should('contain.text', "This field is required");
+      }
+  );
+  });
+
+
      //Nombre de la prueba: Editar Learniing Path con espacio en blanco en VERSION y con NAME completo Boton Save desabilitado
-  //Codigo: CP-54
+  //Codigo: CP-59
   it('Editar Learniing Path con espacio en blanco en VERSION y con NAME completo Boton Save desabilitado', function () {
     cy.get('#email').type('qatec2022@gmail.com');
     cy.get('#password').type('qatecjnsc2022');
@@ -39,6 +62,33 @@ describe('Learning Paths', function () {
 
     cy.get('.btn.btn-company-primary.btn-block').should('be.disabled');
   });
+
+      //Nombre de la prueba: Editar Learniing Path con espacio en blanco en VERSION y con NAME completo, Solicita la VERSION
+  //Codigo: CP-59
+  it('Editar Learniing Path con espacio en blanco en VERSION y con NAME completo, Solicita la VERSION', function () {
+    cy.get('#email').type('qatec2022@gmail.com');
+    cy.get('#password').type('qatecjnsc2022');
+    cy.get('.btn > .m-0').click();
+    cy.get('.list-group > [href="#LearningPathMenu"]').click();
+    cy.get(
+      '.list-group > #LearningPathMenu > [href="/learning_paths"]'
+    ).click();
+
+    cy.get('.row > :nth-child(1) .card-body  > .btn.btn-company-primary > .fas.fa-pen').click()
+
+    cy.get('.card-body > :nth-child(2) #version').clear().then(
+
+      function(){
+          cy.get('.row > :nth-child(1) .card-body > :nth-child(2)  .invalid-feedback').should('contain.text', "This field is required");
+      }
+  );
+
+  });
+
+
+
+
+
 
   afterEach(function () {
     cy.reload();
