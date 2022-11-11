e9c9132debaa76e11a973841695eecedb2432d25
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 21:50:19 -0600

    Fixes

--- cypress/e2e/Bots.cy.ts
+++ cypress/e2e/Bots.cy.ts
@@ -4,23 +4,6 @@ describe('Bots', function () {
     });
 
     /*
-    Nombre de la prueba: Name con espacio en blanco en Bots
-    Objetivo: Probar la funcionalidad de agregar un bot sin nombre
-    Datos de prueba: Name: ----
-    Resultado esperado: Se espera que se pueda registrar el bot con exito.
-    */
-    it('Name con espacio en blanco en Bots', function () {
-        cy.get('#email').type('qatec2022@gmail.com');
-        cy.get('#password').type('qatecjnsc2022');
-        cy.get('.btn > .m-0').click();
-
-        cy.get('.list-group > [href="#securityMenu"] > .d-flex').click();
-        cy.get('.list-group > #securityMenu > [href="/bots"]').click();
-
-        cy.get('.col-md-6 > :nth-child(1)').click()
-    });
-
-    /*
     Nombre de la prueba: Bot sin Permissions
     Objetivo: Probar la funcionalidad de agregar un bot sin permissions
     Datos de prueba: Permissios: ----
@@ -44,6 +27,23 @@ describe('Bots', function () {
                 cy.get('.invalid-feedback').should('contain.text', "This field is required");
             }
         );*/
+    });
+    
+    /*
+    Nombre de la prueba: Name con espacio en blanco en Bots
+    Objetivo: Probar la funcionalidad de agregar un bot sin nombre
+    Datos de prueba: Name: ----
+    Resultado esperado: Se espera que se pueda registrar el bot con exito.
+    */
+    it('Name con espacio en blanco en Bots', function () {
+        cy.get('#email').type('qatec2022@gmail.com');
+        cy.get('#password').type('qatecjnsc2022');
+        cy.get('.btn > .m-0').click();
+
+        cy.get('.list-group > [href="#securityMenu"] > .d-flex').click();
+        cy.get('.list-group > #securityMenu > [href="/bots"]').click();
+
+        cy.get('.col-md-6 > :nth-child(1)').click()
     });
 
     afterEach(function () {
