23bf3104328e456adf8df9bbe4e0fc942a524eb8
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 21:58:14 -0600

    Fixes

--- cypress/e2e/Margin Calculator.cy.ts
+++ cypress/e2e/Margin Calculator.cy.ts
@@ -5,7 +5,7 @@ describe('Margin Calculator', function () {
 
     /*
     Nombre de la prueba: Description con espacio en blanco en Margin Calculator
-    Objetivo: Probar la funcionalidad al agregar un dato en Estimations
+    Objetivo: Probar la funcionalidad del modulo Magin Calculator
     Datos de prueba: 
     Resultado esperado: Que la prueba genere un aviso de que se requiere llenar la Descripción
     */
