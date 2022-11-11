6d5b79493dff01596e4738f9d484685c4490b6a7
Josué Quesada Jiménez <100427564+jQuesada06@users.noreply.github.com>
2022-11-10 21:55:11 -0600

    Fixes

--- cypress/e2e/Estimations.cy.ts
+++ cypress/e2e/Estimations.cy.ts
@@ -26,8 +26,12 @@ describe('Estimations', function () {
         );
     });
 
-    //Nombre de la prueba: Description con un nombre en Estimation
-    //Codigo: CP-13
+    /*
+    Nombre de la prueba: Description con un nombre en Estimation
+    Objetivo: Probar la funcionalidad al agregar un dato en Estimations
+    Datos de prueba: ----
+    Resultado esperado: Que la prueba genere un aviso de que se requiere llenar la Descripción
+    */
     it('Description con un nombre en Estimation', function () {
         cy.get('#email').type('qatec2022@gmail.com');
         cy.get('#password').type('qatecjnsc2022');
